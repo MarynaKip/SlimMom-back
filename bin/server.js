b938adcda3970fe1954b65936e74c05eea696a01
Olha Pylypova <olhasve@gmail.com>
2021-07-24 11:34:06 +0300

    fix  server connection

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,23 @@
 const app = require('../app')
-
+require("dotenv").config();
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+const { connectMongo } = require('../src/db/connection')
+
+const start = async () => {
+  try {
+    await connectMongo()
+
+    app.listen(PORT, (err) => {
+      if (err) {
+        console.error("Error at server launch:", err);
+      }
+      console.log(`Database connection successful at port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(`Failed to launch application with error ${err.message}`)
+    process.exit(1)
+  }
+}
+
+start()
